1095c81889b25db5a075269a439cafa97b6cfd0f
Özlem Güncan <ozlem.guncan@fit.corp>
2019-03-27 16:42:21 +0300

    adding note

--- JUnitCourse/src/com/junit/note/JunitNote.java
+++ JUnitCourse/src/com/junit/note/JunitNote.java
@@ -1,5 +1,23 @@
 package com.junit.note;
 
+
 public class JunitNote {
+	
+	/*Junit nedir?
+	 * 
+	 *Unit test, yazılımda en temel test çeşitidir.
+	 *Yazılan fonksiyonların çalışıp çalışmadığının testlerini yapmak için kullanılır.
+	 *Buradaki en temel noktalardan biri, oluşturulan bütün testler birbirinden bağımsız çalışmaktadır ve her testin tek bir sonucu olmalıdır
+	 * (true / false).
+	 * Unit testlerde en büyük avantaj yazılımın geliştirilme hızıdır.
+	 * Unit testlerin olmadığı bir projeyi düşünelim. Bu projede kullanıcametrelerde gönderilip verilen hataların düzenlenmesidir. Örneğin bir fonksiyon içinde bir yerde bölme işlemi geçiyorsa parametreden bu bölme işleminde böleni 0 yapan bir değer gönderdiğimizde hata vereceğini biliyorsak bu hatanın sonucunda sistemin nasıl bir sonuç üreteceğini de görmemiz gerekmektedir. Bunun için de bir test yazmamız gerekecektir. Classlarımız tamamlandığında testlerinin de oluşturulup classlardaki tüm fonksiyonların testlerini başarıyla tamamlamalarını sağladıktan sonra bir sonraki class’ı kodlamaya başlamak gerekmektedir.
+	 * Yazılan kodun her satırının başka bir kod (test kodu) tarafından otomatik olarak test edilmesini sağlar.
+	 * Kodun anlaşılmasını kolaylaştırır.
+	 * Daha hızlı yazılım geliştirmeyi sağlar.(Proje başlangıcında projeyi yavaşlatır gibi görünmekte fakat projenin ileri dönemlerinde ciddi bir zaman kazanımı sağlamaktadır)
+	 * Koddaki hata oranını azaltır.
+	 * Kodların kalitesinin artmasını sağlar.
+	 * Hataların çabuk tespit edilip düzenlenmesini sağlar.
+	
+   */
 
 }
